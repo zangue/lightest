9b4e67eb21227b3da8ba94513509326b3df26938
Armand Zangue <armand.zangue@gmail.com>
2016-01-26 23:45:12 +0100

    Begin implementation of middleware

---
+++ lightest/Middleware.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Middleware.php
+ *
+ * @author A. Zangue <armand.zangue@gmail.com>
+ */
+
+namespace Lightest;
+
+use Exception;
+
+class Middleware {
+
+	public function __construct()
+	{
+		
+	}
+
+}
