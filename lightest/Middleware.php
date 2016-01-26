783a67a24716bd536f8623fe53ea674e046c4caa
Armand Zangue <armand.zangue@gmail.com>
2016-01-27 00:46:03 +0100

    first implementation of middleware

--- lightest/Middleware.php
+++ lightest/Middleware.php
@@ -12,9 +12,58 @@ use Exception;
 
 class Middleware {
 
+	/**
+	 * @var array
+	 */
+	protected $middlewares;
+
+	/**
+	 * Class constructor
+	 */
 	public function __construct()
 	{
-		
+		$this->middlewares = array();
+	}
+
+	/**
+	 * add a middleware
+	 * @param Object $middleware middleware object
+	 */
+	public function add($middleware)
+	{
+		if (method_exists($middleware, 'before') ||
+			method_exists($middleware, 'after')) {
+			$this->middlewares[] = $middleware;
+			return;
+		}
+
+		throw new Exception("Middleware class must contain at least one of the following methods: before() and after");
+	}
+
+	public function before()
+	{
+		if (empty($this->middlewares))
+			return;
+
+		foreach ($this->middlewares as $middleware) {
+			$middleware->before();
+		}
+
+		return;
+	}
+
+	public function after()
+	{
+		if (empty($this->middlewares))
+			return;
+
+		$middlewares = array_reverse($this->middlewares);
+
+		foreach ($middlewares as $middleware) {
+			$middleware->after();
+		}
+
+		return;
 	}
 
 }
